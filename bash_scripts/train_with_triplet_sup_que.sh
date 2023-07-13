eebccdd631fa8f0c19d831ada7bede628d3a23db
imran2205 <imrankabir1996@gmail.com>
2023-07-14 03:37:26 +0600

    Initial commit

---
+++ bash_scripts/train_with_triplet_sup_que.sh
@@ -0,0 +1,5 @@
+shot=$1
+split=$2
+echo "../tools/train.py --config ../experiments_iu/triplet_sup_que_config/${shot}_shot/uw_few_shot_training_config_triplet_sup_que_shot_${shot}_split_${split}.yaml"
+python3 ../tools/train.py --config ../experiments_iu/triplet_sup_que_config/${shot}_shot/uw_few_shot_training_config_triplet_sup_que_shot_${shot}_split_${split}.yaml
+
