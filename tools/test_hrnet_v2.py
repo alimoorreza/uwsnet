eeb157a88edfc3e9bbe0cb312227584990432b1e
imran2205 <imrankabir1996@gmail.com>
2023-07-25 04:44:06 +0600

    Initial commit

--- tools/test_hrnet_v2.py
+++ tools/test_hrnet_v2.py
@@ -19,7 +19,7 @@ from networks import hrnet_v2 as models
 from config import config_hrnet_v2 as config
 from config import update_config_hrnet_v2 as update_config
 from core.criterion import CrossEntropy, OhemCrossEntropy
-from core.function import validate
+from core.function import validate, testval, test
 from utils.hrnet_v2_utils.utils import create_logger, FullModel
 from utils.hrnet_utils.normalization_utils import get_imagenet_mean_std
 from semantic_dataloader import UWFSDataLoader
